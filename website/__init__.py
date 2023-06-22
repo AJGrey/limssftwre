e59ea36f9e740ea05add799895cdbb184b581ec1
AJGrey <greyajayi@yahoo.com>
2023-06-23 00:51:28 +0100

    auth&views

--- website/__init__.py
+++ website/__init__.py
@@ -1,56 +1,66 @@
-"""
-This module serves as the entry point for the website.
-It initializes the Flask application, sets up the database,
-and registers the blueprints for different components of the
-website. The `create_app` function creates and configures the
-Flask application instance, sets up the database connection
-and registers the necessary blueprints. The `db` object is an
-instance of SQLAlchemy, which provides an interface to interact
-with the database. The `views` and `auth` blueprints define the 
-routes and views for the main functionality and authentication
-of the website. To start the website, run the `create_app`
-function to get the Flask application instance and then run it 
-using the `run()` method.
-
-"""
-
 from os import path
-from flask import Flask
+from flask import Flask, session, g, request, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-
-
+from flask_bcrypt import Bcrypt
+from .auth import auth
 
 db = SQLAlchemy()
-DB_NAME = "database.db"
-
+bcrypt = Bcrypt()
 
 def create_app():
-    """Creating the app"""
+    """Create and configure the flask application instance"""
     app = Flask(__name__)
+    DB_NAME = "database.db"
     app.config['SECRET_KEY'] = 'manny manny'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
+    db.app = app
 
+    # Importing and registering blueprints
     from .views import views
     from .auth import auth
-
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User, Client, ClinicalData
+    # Initialize bcrypt
+    bcrypt.init_app(app)
 
-    with app.app_context():
-        db.create_all()
+    # Custom user authentication logic
+    from .models import User
+    
+    def create_database(app):
+        if not path.exists('website/' + DB_NAME):
+            db.create_all(app=app)
 
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
 
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        if request.method == 'POST':
+            email = request.form.get('email')
+            password = request.form.get('password')
 
+            user = User.query.filter_by(email=email.lower()).first()
+            if user and bcrypt.check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                session['user_id'] = user.id
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect email or password, try again.', category='error')
+
+        return render_template("login.html")
+
+    @app.route('/logout')
+    def logout():
+        session.pop('user_id', None)
+        return redirect(url_for('views.home'))
+
+    @app.before_request
+    def load_logged_in_user():
+        user_id = session.get('user_id')
+        if user_id:
+            g.user = User.query.get(user_id)
+        else:
+            g.user = None
 
     return app
 
@@ -58,4 +68,3 @@ def create_app():
 def create_database(app):
     if not path.exists('website/' + DB_NAME):
         db.create_all(app=app)
-        print('Created Database!')
--- website/auth.py
+++ website/auth.py
@@ -1,20 +1,21 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_required, current_user
-from .models import User
-from . import db   ##means from __init__.py import db
+from flask_sqlalchemy import SQLAlchemy
+from wtforms.validators import InputRequired
+from flask_login import login_user, logout_user, login_required, current_user
 
 
 auth = Blueprint('auth', __name__, static_folder='static')
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
+    from .models import User
+    from . import db # Import db locally within the function
     if request.method == 'POST':
-        firstname = request.form.get('fname')
-        lastname = request.form.get('lname')
+        firstname = request.form.get('first_name')
+        lastname = request.form.get('last_name')
         email = request.form.get('email')
         department = request.form.get('dept')
-        password = request.form.get('pin')
+        password = request.form.get('password')
 
         user = User.query.filter_by(email=email).first()
         if user:
@@ -25,11 +26,11 @@ def register():
             flash('First name must be greater than 1 character.', category='error')
         elif len(lastname) < 2:
             flash('Last name must be greater than 1 character.', category='error')
-        elif len(password) != 3:
+        elif len(password) != 4:
             flash('Pin must be 4 characters.', category='error')
         else:
-            new_user = User(email=email, firstname=firstname, pin=generate_password_hash(
-                password, method='sha256'))
+            hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+            new_user = User(email=email, firstname=firstname, lastname=lastname, department=department, password=hashed_password)
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
@@ -38,22 +39,25 @@ def register():
 
     return render_template("register.html", user=current_user)
 
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    from .models import User
+    from . import db, bcrypt # Import db and bcrypt lcoally
     if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('pin')
+        email = request.form.get('email')
+        password = request.form.get('password')
 
-        user = User.query.filter_by(username=username).first()
+        user = User.query.filter_by(email=email).first()
         if user:
-            if check_password_hash(user.pin, password):
+            if bcrypt.check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
-                flash('Incorrect pin, try again.', category='error')
+                flash('Incorrect password, try again.', category='error')
         else:
-            flash('Username does not exist.', category='error')
+            flash('Email does not exist.', category='error')
 
     return render_template("login.html", user=current_user)
 
@@ -62,4 +66,4 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('auth.login'))
+    return redirect(url_for('views.home'))
